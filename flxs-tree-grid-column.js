2da030af5ed59e650de0ef297fc1e9d11ff3aa5c
Flexicious <flexicious@gmail.com>
2017-06-20 14:48:32 -0400

    Slot Item Renderer support

--- flxs-tree-grid-column.js
+++ flxs-tree-grid-column.js
@@ -1,4 +1,36 @@
 (function () {
+  //SlotItemRenderer
+    /**
+     * A SlotItemRenderer is a custom item renderer, that defines how to use custom cells with logic that you can 
+     * embed in slot of the column
+     * @constructor
+     * @namespace com.flexicious.controls
+     * @extends UIComponent
+     */
+    var SlotItemRenderer=function(template){
+        //make sure to call constructor
+        flexiciousNmsp.UIComponent.apply(this,["div"]);//second parameter is the tag name for the dom element.
+        /**
+         * This is a getter/setter for the data property. When the cell is created, it belongs to a row
+         * The data property points to the item in the grids dataprovider that is being rendered by this cell.
+         * @type {*}
+         */
+        this.data=null;
+        //we can use innerHTML in the constructor - this makes subsequent setData calls not recreate the dom tree.
+        this.setDomElement(template.cloneNode(true));
+    };
+    flexiciousNmsp.SlotItemRenderer = SlotItemRenderer; //add to name space
+    SlotItemRenderer.prototype = new flexiciousNmsp.UIComponent(); //setup hierarchy
+    SlotItemRenderer.prototype.typeName = SlotItemRenderer.typeName = 'SlotItemRenderer';//for quick inspection
+    SlotItemRenderer.prototype.getClassNames=function(){
+        return ["SlotItemRenderer","UIComponent"]; //this is a mechanism to replicate the "is" and "as" keywords of most other OO programming languages
+    };
+
+    //This sets  the inner html, and grid will try to set it. Since we are an input field, IE 8 will complain. So we ignore it since we dont need it anyway.
+    SlotItemRenderer.prototype.setText=function(val){
+
+    };
+  //End SlotItemRenderer
 
   var template = new flexiciousNmsp.FlexDataGridColumn();
   var gridPropsAndBehaviors = flexiciousNmsp.SettingsParser.getPropertiesAndBehaviors(template);
@@ -13,7 +45,19 @@
     attached: function () {
 
       this.gridColumn = this.getAttribute("type") == "checkbox" ? new flexiciousNmsp.FlexDataGridCheckBoxColumn() : new flexiciousNmsp.FlexDataGridColumn();
-
+      if (this.firstChild) {
+        var node = this.firstChild;
+        var realNode = null;
+        while (node) {
+          if (node.nodeName != "#text") {
+            realNode = node;
+          }
+          node = node.nextSibling;
+        }
+        if(realNode){
+            this.gridColumn.itemRenderer=(new flexiciousNmsp.ClassFactory(SlotItemRenderer, realNode, true));
+        }
+      }
       if (this.parentNode.gridColumnGroup) {
         if (!this.parentNode.gridColumnGroup._tempCols) {
           this.parentNode.gridColumnGroup._tempCols = [];
@@ -44,4 +88,4 @@
     _onChanged: function () {
     }
   });
-} ());
+}());

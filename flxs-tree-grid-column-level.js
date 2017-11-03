62462d052ec30c36a9712bea462e33cd228e579e
flexicioussupport <support@flexicious.com>
2017-11-03 16:48:49 +0530

    Update flxs-tree-grid-column-level.js

--- flxs-tree-grid-column-level.js
+++ flxs-tree-grid-column-level.js
@@ -10,36 +10,26 @@
     behaviors: [
       behaviors
     ],
-    attached: function () {
-
-      this.gridColumnLevel = this.parentNode.grid ? this.parentNode.grid.getColumnLevel() : new flexiciousNmsp.FlexDataGridColumnLevel();
-      this.gridColumnLevel.cellBorderFunction = this._cellBorderFunction;
-      this.async(function () {
-        for (var key in properties) {
-          if (this[key.toLowerCase()]) {
-            ((this.parentNode.grid || this.gridColumnLevel.grid)).applyAttribute(this.gridColumnLevel, properties[key].orig, this[key.toLowerCase()], true);
-          }
+    created: function () {
+      flexiciousNmsp.SettingsParser.log("level created")
+    },
+    ready: function () {
+      flexiciousNmsp.SettingsParser.log("level ready")
+    },
+    parseLevel: function (gridColumnLevel, grid) {
+      for (var key in properties) {
+        if (this[key.toLowerCase()]) {
+          grid.applyAttribute(gridColumnLevel, properties[key].orig, this[key.toLowerCase()], true);
         }
-
-        // if (this.gridColumnLevel._tempCols && this.gridColumnLevel._tempCols.length) {
-        //   this.gridColumnLevel.setColumns(this.gridColumnLevel._tempCols);
-        // }
-      }, 1);
-
-      if (this.parentNode.grid) {
-
-      } else {
-        this.parentNode.gridColumnLevel.nextLevel = (this.gridColumnLevel);
       }
     },
-
     attributeChanged: function () {
       this._onChanged();
     },
 
     _onChanged: function () {
     },
-    
+
     _cellBorderFunction: function (cell) {
       if (cell.rowInfo.getIsDataRow()) {
         if (cell.rowInfo.rowPositionInfo.levelNestDepth == 1) {
